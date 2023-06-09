9489de322c849f4ecffbdd52d75915df805e262e
sindhurajain <sindhura394@gmail.com>
2023-06-10 01:24:16 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header/Header'
+
+
+
+function App() {
+  return (
+
+     <div className="App">
+        <Header />
+        <img id = "mrtmap" src="mrtmap2.png" alt="Mrt Map"/>
+    </div>
+    
+  );
+}
+
+export default App;
